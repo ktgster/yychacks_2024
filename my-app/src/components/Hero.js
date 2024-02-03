afc0cc49598046f3ff04b6b76e1a9a7fe297f5b1
Eric Day <eric.ch.day@gmail.com>
2024-02-03 16:35:32 -0700

    landing page tweaks

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Hero() {
 	return (
-		<div className="bg-[url('../public/images/hero-image.jpg')] bg-cover flex flex-col items-center justify-center" 
-			 style={{minHeight: "100vh", position: "relative"}} 
+		<div className="bg-[url('../public/images/hero-image.jpg')] bg-cover flex flex-col items-center justify-center"
+			 style={{minHeight: "100vh", position: "relative"}}
 		>
 			<div className="w-full h-screen bg-gradient-to-b from-black via-transparent to-transparent absolute"></div>
 			<h1 className="relative mt-16 pb-8 text-7xl font-bold tracking-normal text-center text-white max-md:mt-10 max-md:max-w-full max-md:text-4xl">
@@ -14,7 +14,9 @@ export default function Hero() {
 		      <br />
 		      Connecting Immigrants with Welcoming Host Families.
 		    </div>
-		    <button className="relative justify-center px-10 py-3.5 mt-8 mb-32 text-3xl font-bold tracking-wide text-center text-white whitespace-nowrap bg-[#c8102f] rounded-[100px] max-md:px-5 max-md:mb-10">Create Account</button>
+		    <button className="relative justify-center hover:shadow-neutral-900 shadow-lg hover:scale-105 px-10 py-3.5 mt-8 mb-32 text-3xl font-bold tracking-wide text-center text-white whitespace-nowrap bg-[#c8102f] rounded-[100px] max-md:px-5 max-md:mb-10"
+								style={{ transition: "0.35s ease-in-out" }}
+				>Create Account</button>
 			{/*"#c810df"*/}
 		</div>
 	)
