e4734dd4204af0176d9c7f555679b37ff1768567
Your Name <you@example.com>
2024-02-03 16:04:00 -0700

    combining hero with navigation & adding figma styling

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 export default function Hero() {
 	return (
-		<div className="bg-[url('../public/images/hero-image.jpg')] bg-cover flex items-center justify-center" 
+		<div className="bg-[url('../public/images/hero-image.jpg')] bg-cover flex flex-col items-center justify-center" 
 			 style={{minHeight: "100vh", position: "relative"}} 
 		>
-			<div className="w-full h-screen bg-black bg-opacity-60 absolute"></div>
-			<h1 className="text-center text-8xl text-white font-bold z-10 opacity-80">Welcome to Calgary</h1>
+			<div className="w-full h-screen bg-gradient-to-b from-black via-transparent to-transparent absolute"></div>
+			<h1 className="relative mt-16 pb-8 text-7xl font-bold tracking-normal text-center text-white max-md:mt-10 max-md:max-w-full max-md:text-4xl">
+				Hosts Welcoming Hearts
+			</h1>
+			<div className="relative text-xl font-medium tracking-wide text-center text-white max-md:max-w-full">
+		      Find Your Home Away from Home:
+		      <br />
+		      Connecting Immigrants with Welcoming Host Families.
+		    </div>
+		    <button className="relative justify-center px-10 py-3.5 mt-8 mb-32 text-3xl font-bold tracking-wide text-center text-white whitespace-nowrap bg-[#c8102f] rounded-[100px] max-md:px-5 max-md:mb-10">Create Account</button>
+			{/*"#c810df"*/}
 		</div>
 	)
 }
