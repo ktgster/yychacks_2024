db84d3799af832066a25b6cd826298348f7967ed
Manpreet Singh <40501907+montylubana09@users.noreply.github.com>
2024-02-03 19:25:58 -0700

    Manpreet addition (#25)
    * fixed hover on create account
    
    * deleted signup
    
    * sample image deleted
    
    * changed information in footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -3,15 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer
-            className="text-center text-lg-start text-white bg-gradient-to-t from-black via-black to-transparent mt-20"
-      >
-   
-        <section
-                className="d-flex justify-content-between p-4 bg-transparent">
-       
-          
-         
+      <footer className="text-center text-lg-start text-white bg-gradient-to-t from-black via-black to-transparent mt-20">
+        <section className="d-flex justify-content-between p-4 bg-transparent">
           <div className="ml-auto">
             <a href="#!" className="text-white me-4">
               <i className="fab fa-facebook-f"></i>
@@ -29,26 +22,28 @@ function Footer() {
               <i className="fab fa-linkedin"></i>
             </a>
           </div>
-       
         </section>
-    
+
         <section className="">
           <div className="container text-center text-md-start mt-5">
-          
             <div className="row mt-3">
-            
               <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-               
                 <h6 className="text-uppercase fw-bold">Your Perfect Day!</h6>
                 <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{width: "60px", backgroundColor: "transparent", height: "2px"}}
-                    />
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "transparent",
+                    height: "2px",
+                  }}
+                />
                 <p>
-                  Welcome to YYC Winter! Our mission is to provide the best services for you to discover and experience our great city. Please feel free to connect with us.
+                  Welcome to DomiYYC! Our mission is to provide the best
+                  services for you to discover and experience our great city.
+                  Please feel free to connect with us.
                 </p>
               </div>
-             
+
               {/* <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               
                 <h6 className="text-uppercase fw-bold">Products</h6>
@@ -69,52 +64,78 @@ function Footer() {
                   <a href="#!" className="text-white">Bootstrap Angular</a>
                 </p>
               </div> */}
-        
+
               <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-               
                 <h6 className="text-uppercase fw-bold">Useful links</h6>
                 <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{width: "60px", backgroundColor: "#7c4dff", height: "2px"}}
-                    />
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "#7c4dff",
+                    height: "2px",
+                  }}
+                />
                 <p>
-                  <a href="#!" className="text-white">Your Account</a>
+                  <a href="#!" className="text-white">
+                    Partners
+                  </a>
                 </p>
                 <p>
-                  <a href="#!" className="text-white">Become a Member</a>
+                  <a href="#!" className="text-white">
+                    Become a Host
+                  </a>
                 </p>
                 <p>
-                  <a href="#!" className="text-white">Help</a>
+                  <a href="#!" className="text-white">
+                    Help
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-white">
+                    Privacy Policy
+                  </a>
+                </p>
+                <p>
+                  <a href="#!" className="text-white">
+                    Cookies Policy
+                  </a>
                 </p>
               </div>
-             
+
               <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              
                 <h6 className="text-uppercase fw-bold">Contact</h6>
                 <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{width: "60px", backgroundColor: "transparent", height: "2px"}}
-                    />
-                <p><i className="fas fa-home mr-3"></i> Calgary, Alberta, Canada</p>
-                <p><i className="fas fa-envelope mr-3"></i> info@yycwinter.ca</p>
-                <p><i className="fas fa-phone mr-3"></i> + 01 234 5678</p>
-                <p><i className="fas fa-print mr-3"></i> + 01 234 5678</p>
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{
+                    width: "60px",
+                    backgroundColor: "transparent",
+                    height: "2px",
+                  }}
+                />
+                <p>
+                  <i className="fas fa-home mr-3"></i> Calgary, Alberta, Canada
+                </p>
+                <p>
+                  <i className="fas fa-envelope mr-3"></i> info@domiyyc.ca
+                </p>
+                <p>
+                  <i className="fas fa-phone mr-3"></i> +1 403 000 0000
+                </p>
+                <p>
+                  <i className="fas fa-print mr-3"></i> +1 403 000 0001
+                </p>
               </div>
-         
             </div>
-           
           </div>
         </section>
-      <div
-           className="text-center p-3"
-           style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
-           >
-            © 2024 Copyright Hosts Welcoming Hearts
-      </div>
-   
-    </footer>
-
-  </div>
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2024 Copyright Hosts Welcoming Hearts
+        </div>
+      </footer>
+    </div>
   );
 }
 export default Footer;
